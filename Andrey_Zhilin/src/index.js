c7da352a3bd6ca2fb57a9b2dc59a15982dcb76f6
Жилин Андрей <152083348+tengenatari@users.noreply.github.com>
2024-10-20 20:13:35 +0500

    added toasts
    toasts

--- Andrey_Zhilin/src/index.js
+++ Andrey_Zhilin/src/index.js
@@ -1,2 +1,14 @@
 console.log("Hello World!");
 import './style.sass';
+import * as bootstrap from 'bootstrap'
+var $ = require('jquery');
+
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
