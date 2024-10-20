5c01401486d130ddafe47f9ce325f957b13fb922
Жилин Андрей <152083348+tengenatari@users.noreply.github.com>
2024-10-20 22:03:45 +0500

    js and popovers
    added js for modals and added popovers

--- Andrey_Zhilin/src/index.js
+++ Andrey_Zhilin/src/index.js
@@ -1,10 +1,16 @@
 console.log("Hello World!");
 import './style.sass';
+
 import * as bootstrap from 'bootstrap'
 var $ = require('jquery');
 
 const toastTrigger = document.getElementById('liveToastBtn')
 const toastLiveExample = document.getElementById('liveToast')
+
+$(document).ready(function () {
+    const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
+    const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+});
 
 if (toastTrigger) {
   const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
@@ -12,3 +18,12 @@ if (toastTrigger) {
     toastBootstrap.show()
   })
 }
+
+$(document).on('keydown', '.modal', function(e) {
+    if(e.key=='ArrowRight'){
+        document.getElementById('r-btn' + this.getAttribute('num')).click();
+    }
+    if(e.key=='ArrowLeft'){
+        document.getElementById('l-btn' + this.getAttribute('num')).click();
+    }
+});
